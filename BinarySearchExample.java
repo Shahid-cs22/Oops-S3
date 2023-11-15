61fdb746d1519a68124f76316f733e3e1c64111d
MOHD_SHAHID <146314603+Shahid-cs22@users.noreply.github.com>
2023-11-15 20:08:10 +0530

    BinarySearchExample.java

--- BinarySearchExample.java
+++ BinarySearchExample.java
@@ -1,26 +1,94 @@
-class BinarySearchExample{  
-    public static void binarySearch(int arr[], int first, int last, int key){  
-      int mid = (first + last)/2;  
-      while( first <= last ){  
-         if ( arr[mid] < key ){  
-           first = mid + 1;     
-         }else if ( arr[mid] == key ){  
-           System.out.println("Element is found at index: " + mid);  
-           break;  
-         }else{  
-            last = mid - 1;  
-         }  
-         mid = (first + last)/2;  
-      }  
-      if ( first > last ){  
-         System.out.println("Element is not found!");  
-      }  
-    }  
-    public static void main(String args[]){  
-           int arr[] = {10,20,30,40,50};  
-           int key = 30;  
-           int last=arr.length-1;  
-           binarySearch(arr,0,last,key);     
-    }  
-   }  
-   
+Import java.util.Scanner;
+
+
+
+Public class BinarySearch {
+
+    Public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        System.out.print(“Enter the size of the array: “);
+
+        Int size = scanner.nextInt();
+
+        Int[] array = new int[size];
+
+
+
+        System.out.println(“Enter the sorted elements of the array:”);
+
+        For (int i = 0; i < size; i++) {
+
+            Array[i] = scanner.nextInt();
+
+        }
+
+
+
+        System.out.print(“Enter the element to search: “);
+
+        Int target = scanner.nextInt();
+
+
+
+        Int result = binarySearch(array, target);
+
+
+
+        If (result == -1) {
+
+            System.out.println(“Element not found in the array.”);
+
+        } else {
+
+            System.out.println(“Element found at index: “ + result);
+
+        }
+
+
+
+        Scanner.close();
+
+    }
+
+
+
+    Public static int binarySearch(int[] array, int target) {
+
+        Int left = 0, right = array.length – 1;
+
+
+
+        While (left <= right) {
+
+            Int mid = left + (right – left) / 2;
+
+
+
+            If (array[mid] == target) {
+
+                Return mid;
+
+            } else if (array[mid] < target) {
+
+                Left = mid + 1;
+
+            } else {
+
+                Right = mid – 1;
+
+            }
+
+        }
+
+
+
+        Return -1;
+
+    }
+
+}
+
