5004b457018864aeaf5599f557d2049c0ac988b5
MOHD_SHAHID <146314603+Shahid-cs22@users.noreply.github.com>
2023-11-15 20:11:53 +0530

    LinearSearch1.java

--- LinearSearch1.java
+++ LinearSearch1.java
@@ -1,31 +1,76 @@
-Public class LinearSearch1
-{  
-  Public static void main(String args[])  
-  {  
-Inti, limit, key;  
+Import java.util.Scanner;
 
-    Scanner sc = new Scanner(System.in);  
-System.out.println(“Enter the limit of numbers:”);  
-    Limit = sc.nextInt();   
-Int array[] = new int[limit];
 
-System.out.println(“Enter “ +limit + “ numbers:”);  
 
-    For (i = 0; i< limit; i++)  
-      Array[i] = sc.nextInt();  
+Public class LinearSearch {
 
-System.out.println(“Enter the search element:”);  
-    Key = sc.nextInt();  
+    Public static void main(String[] args) {
 
-    For (i = 0; i< limit; i++)  
-    {  
-      If (array[i] == key)      
-      {  
-System.out.println(“The search element “+ key +” is found at location “+ (i+1));
-          Break;  
-      }  
-   }  
-   If (i == limit)  
-System.out.println(“The search element “+ key +” is not found”);
-  }  
-}  
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        System.out.print(“Enter the size of the array: “);
+
+        Int size = scanner.nextInt();
+
+        Int[] array = new int[size];
+
+
+
+        System.out.println(“Enter the elements of the array:”);
+
+        For (int i = 0; i < size; i++) {
+
+            Array[i] = scanner.nextInt();
+
+        }
+
+
+
+        System.out.print(“Enter the element to search: “);
+
+        Int target = scanner.nextInt();
+
+
+
+        Int result = linearSearch(array, target);
+
+
+
+        If (result == -1) {
+
+            System.out.println(“Element not found in the array.”);
+
+        } else {
+
+            System.out.println(“Element found at index: “ + result);
+
+        }
+
+
+
+        Scanner.close();
+
+    }
+
+
+
+    Public static int linearSearch(int[] array, int target) {
+
+        For (int i = 0; i < array.length; i++) {
+
+            If (array[i] == target) {
+
+                Return i;
+
+            }
+
+        }
+
+        Return -1;
+
+    }
+
+}
+
